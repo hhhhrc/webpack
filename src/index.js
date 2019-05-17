8ac9119dfdb2622a555a660b68dc061997221775
hhhhrc <1270660449@qq.com>
2019-05-17 23:28:08 +0800

    3-4

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import style from './index.scss';
+import avatar from './avatar.jpg';
+
+import createAvatar from './createAvatar';
+
+createAvatar();
+
+var img = new Image();
+img.src = avatar;
+img.classList.add(style.avatar);
+
+var root = document.getElementById('root');
+root.append(img);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js'
+	},
+	module: {
+		rules: [{
+			test: /\.(jpg|png|gif)$/,
+			use: {
+				loader: 'url-loader',
+				options: {
+					name: '[name]_[hash].[ext]',
+					outputPath: 'images',
+					limit: 10240
+				}
+			}
+		}, {
+			test: /\.scss$/,
+			// 后者根据关系整合CSS文件，前者挂载到head的style中
+			//loader执行顺序：从下到上，从右到左
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						// 解决SCSS中import进来的scss文件不走下面两个loader
+						importLoaders: 2,
+						// 开启CSS模块化打包
+						modules: true
+					}
+				},
+				'sass-loader',
+				// 自动添加厂商前缀
+				'postcss-loader'
+			]
+		}],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
